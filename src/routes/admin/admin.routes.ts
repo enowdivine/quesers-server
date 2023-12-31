0b95631ede21d3bd5910ee437b9681c5ee5af297
enowdivine <sirdivine16@gmail.com>
2023-12-31 17:53:17 +0100

    s3 test working

--- src/routes/admin/admin.routes.ts
+++ src/routes/admin/admin.routes.ts
@@ -5,9 +5,9 @@ import AdminMiddleware from "../../middleware/auth/admin";
 const router = express.Router();
 const admin = new AdminAuth();
 
-router.post("/register", AdminMiddleware, admin.register);
+router.post("/register", admin.register);
 router.post("/login", admin.login);
-router.put("/update/:id", admin.update);
+router.put("/update/:id", AdminMiddleware, admin.update);
 router.put("/update-password/:id", admin.updatePassword);
 
 export default router;
