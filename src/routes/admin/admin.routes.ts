147e74a26884deadc0ed636edb350e5853d2b275
enowdivine <sirdivine16@gmail.com>
2024-01-25 08:56:07 +0100

    error fix

--- src/routes/admin/admin.routes.ts
+++ src/routes/admin/admin.routes.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import AdminAuth from "./admin.controller";
-import AdminMiddleware from "../../middleware/auth/admin";
+import VendorMiddleware from "../../middleware/auth/vendor";
 
 const router = express.Router();
 const admin = new AdminAuth();
 
 router.post("/register", admin.register);
 router.post("/login", admin.login);
-router.put("/update/:id", AdminMiddleware, admin.update);
-router.put("/update-password/:id", admin.updatePassword);
+router.put("/update/:id", VendorMiddleware, admin.update);
+router.put("/update-password/:id", VendorMiddleware, admin.updatePassword);
 
 export default router;
